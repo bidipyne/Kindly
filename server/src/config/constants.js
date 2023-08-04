98a04269ac679de70bf2a775f707c5fa646808f8
Parwat Kunwar <parwatkunwar08@gmail.com>
2023-08-03 20:26:07 -0400

    Add constants

--- server/src/config/constants.js
+++ server/src/config/constants.js
@@ -2,3 +2,6 @@ export const PORT = 3001;
 export const HOST = 'http://127.0.0.1';
 
 export const saltRounds = 10;
+
+export const VOLUNTEER = 'volunteer';
+export const ORGANIZATION = 'organization';
