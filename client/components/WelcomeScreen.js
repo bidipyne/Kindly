ee9d4830bb46f8fa236d80a5cd0bf7e5e31f6aa7
Parwat Kunwar <parwatkunwar08@gmail.com>
2023-08-13 14:19:03 -0400

    Fix login screen ui issues

--- client/components/WelcomeScreen.js
+++ client/components/WelcomeScreen.js
@@ -4,14 +4,16 @@ import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
 const WelcomeScreen = ({ navigation }) => {
   return (
     <View style={styles.container}>
-      <Text style={styles.welcomeText}>Welcome!</Text>
-      <Text style={styles.welcomeText}>Let's get started</Text>
+      <View style={styles.welcomeTextWrapper}>
+        <Text style={styles.welcomeText}>Welcome!</Text>
+        <Text style={styles.welcomeText}>Let's get started</Text>
+      </View>
       <Text style={styles.descriptionText}> Select your way to be Kind</Text>
       <TouchableOpacity style={styles.button} onPress={() => navigation.navigate('NonProfitSignupScreenOne')}>
-        <Text style={styles.buttonText}>Non-Profit Signup</Text>
+        <Text style={styles.buttonText}>For Non-profits</Text>
       </TouchableOpacity>
       <TouchableOpacity style={styles.button} onPress={() => navigation.navigate('VolunteerSignup')}>
-        <Text style={styles.buttonText}>Volunteer Signup</Text>
+        <Text style={styles.buttonText}>Volunteer / Donate</Text>
       </TouchableOpacity>
       <Text
         style={styles.loginLink}
@@ -30,18 +32,19 @@ const styles = StyleSheet.create({
     padding: 16,
     backgroundColor: '#F5FCFF',
   },
+  welcomeTextWrapper: {
+    marginVertical: 40
+  },
   welcomeText: {
     fontSize: 30,
-    fontWeight: '600',
+    fontWeight: '500',
     textAlign: 'center',
-    margin: 10,
     color: '#009CE0',
   },
   descriptionText: {
-    fontSize: 18,
+    fontSize: 25,
     textAlign: 'center',
-    margin: 10,
-    // color: '#009CE0',
+    marginBottom: 20,
   },
   button: {
     margin: 10,
@@ -58,10 +61,7 @@ const styles = StyleSheet.create({
     fontSize: 16,
     color: '#000',
     textAlign: 'center',
-    position: 'absolute',
-    bottom: 80,
-    left: 0,
-    right: 0,
+    marginTop: 20
   },
 });
 
